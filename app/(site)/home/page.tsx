b32bfc55f9922d989d7c27a494b958820130ebd4
JDubbs89 <152918687+JDubbs89@users.noreply.github.com>
2025-09-18 00:34:38 -0400

    Initial commit for test branch, removed all excess body paragraphs from home page

--- app/(site)/home/page.tsx
+++ app/(site)/home/page.tsx
@@ -11,12 +11,6 @@ export default function Home() {
     <ContentContainer title="Home" header="Background" content="Hello, my name is Jonathan! I am a computer science student at Trine University in Angola Indiana.
       I am an aspiring software developer, experienced in Python, JavaScript, SQL, and more."
       />
-    <ContentContainer title="Home" header="Hello World!" content="Some body text to fill in the box."/>
-    <ContentContainer title="Home" header="Hello World!" content="Some body text to fill in the box."/>
-    <ContentContainer title="Home" header="Hello World!" content="Some body text to fill in the box."/>
-    <ContentContainer title="Home" header="Hello World!" content="Some body text to fill in the box."/>
-    <ContentContainer title="Home" header="Hello World!" content="Some body text to fill in the box."/>
-    <ContentContainer title="Home" header="Hello World!" content="Some body text to fill in the box."/>
 
     </div>
   )
