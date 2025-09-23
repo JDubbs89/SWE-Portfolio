e5eca130bac26c64aaf36aa42a9d16f14b9cc5ef
JDubbs89 <152918687+JDubbs89@users.noreply.github.com>
2025-09-23 01:37:53 -0400

    Filled out and styled work experiences

--- app/_components/containers/default/positionContainer.tsx
+++ app/_components/containers/default/positionContainer.tsx
@@ -1,29 +1,41 @@
 
-export default function PositionContainer(){
+type ContainerProps = {
+    startDate?: string;
+    endDate?: string;
+    positionTitle?: string;
+    companyName?: string;
+    location?: string;
+    bullets?: string[];
+    last?: boolean;
+}
+
+export default function PositionContainer(
+    { startDate = "May 2025", endDate = "Present", positionTitle = "Senior Thing Doer", companyName = "Company Inc", location = "Jackson, MI", 
+        bullets = ["Spearheaded complicated project involving keywords, buzzwords, and skills for a 76% increase in productivity, usefulness, and things getting done, by increasing the number of things done per hour to over 6."], 
+        last = false
+    }: ContainerProps
+){
     return (
-        <div className="flex flex-row my-0 py-0 h-full w-fit"> {/* container col */}
+        <div className="flex flex-row my-0 py-0 h-full w-fit">
             <div className="flex flex-col gap-0">
-                <div className="text-[clamp(0.70rem,0.8rem,0.8rem)] font-medium text-gray-400 mx-auto whitespace-nowrap">May 2024</div>
-                <span className="relative h-20 w-0.25 mx-4 my-0 py-0 bg-blue-400"></span> {/* timeline span */}
-                <span className="rounded-full h-2 w-2 ml-3.25 bg-white"></span>
-                <span className="relative h-20 w-0.25 mx-4 my-0 py-0 bg-blue-400"></span> {/* timeline span */}
-                <div className="text-[clamp(0.70rem,0.8rem,0.8rem)] font-medium text-gray-400 mx-auto whitespace-nowrap">May 2025</div>
+                <div className="text-[clamp(0.70rem,0.8rem,0.8rem)] font-medium text-gray-400 ml-2 my-2 whitespace-nowrap">{endDate}</div>
+                <span className="relative h-full w-0.5 mx-4 my-0 py-0 bg-blue-400"></span>
+                <span className="rounded-full h-5 w-2 ml-3.25 bg-white"></span>
+                <span className="relative h-full w-0.5 mx-4 my-0 py-0 bg-blue-300"></span>
+                {(last ? <div className="text-[clamp(0.70rem,0.8rem,0.8rem)] font-medium text-gray-400 ml-2 my-2 whitespace-nowrap">{startDate}</div> : null)}
             </div>
             
-            <div className="w-full my-auto p-4 bg-gradient-to-br from-[rgba(15,15,20,0.4)] via-[rgba(10,10,10,0.8)] to-[rgba(5,41,158,0.05)] border border-[rgba(83,134,228,0.2)] rounded-[clamp(0.75rem,2vw,1rem)]">
-                <div className="text-[clamp(1.25rem,1.5rem,1.5rem)] font-semibold">Senior Thing Doer</div>
-                <div className="text-[clamp(0.70rem,0.9rem,0.9rem)] font-medium text-blue-400 ml-2">Company Inc, Jackson, MI</div>
-                <div className="text-[clamp(0.70rem,0.8rem,0.8rem)] font-medium text-gray-400 ml-2">May 20xx - Apr 20xx</div>
-                <div className="flex flex-col gap-2">
-                    <div className="flex gap-2">
-                        <span className="rounded-full h-1 w-1 mb-auto mt-2.5 ml-3.25 bg-white"></span>
-                        <p>
-                            Spearheaded complicated project involving keywords, buzzwords, and skills for a 76% increase in productivity, usefulness, and things getting done,
-                            by increasing the number of things done per hour to over 6.
-                        </p>
+            <div className="w-full ml-2 mt-6 mb-4 p-4 bg-gradient-to-br from-[rgba(15,15,20,0.4)] via-[rgba(10,10,10,0.8)] to-[rgba(5,41,158,0.05)] border border-[rgba(83,134,228,0.2)] rounded-[clamp(0.75rem,2vw,1rem)]">
+                <div className="text-[clamp(1.25rem,1.5rem,1.5rem)] font-semibold mb-2">{positionTitle}</div>
+                <div className=" ml-2 mt-2 mb-1 text-[clamp(0.70rem,0.9rem,0.9rem)] font-medium text-blue-400">{companyName} - {location}</div>
+                    <div className="flex flex-col ml-4 mt-3 gap-1 text-[clamp(0.70rem,0.9rem,0.9rem)]/5 font-light text-gray-400">
+                        {bullets.map((bullet, index) => (
+                            <div key={index} className="mb-1">
+                                • {bullet}
+                            </div>
+                        ))}
                     </div>
-                </div>
-            </div> {/* position div */}
+            </div>
         </div> 
     )
 }
--- app/_components/sections/workExpSection.tsx
+++ app/_components/sections/workExpSection.tsx
@@ -5,11 +5,43 @@ import Image from "next/image";
 export default function WrkExpSection(){
     return(
         <BaseSection className="flex justify-center my-5">
-            <div className="flex flex-col w-fit h-full justify-between content-wrapper-px"> {/* col container for containers */}
-                <PositionContainer/>
-                {/* <PositionContainer/>
-                <PositionContainer/> */}
+            
+            <div className="text-5xl font-bold content-wrapper">My Journey
+            <div className="flex flex-col w-fit h-full mt-5 justify-between content-wrapper-px"> {/* col container for containers */}
+                <PositionContainer 
+                    positionTitle="Information Technology Intern"
+                    startDate="May 2025"
+                    endDate="Aug 2025"
+                    companyName="Martinrea International"
+                    location="Jonesville, MI"
+                    bullets={["Spearheaded domain migration of 120+ machine operator kiosks while developing and maintaining detailed documentation, massively reducing network overhead by uniting systems under one corporate policy.",
+                        "Created a robust and easy-to-use PA system for meetings, increasing audibility for listeners farthest from the speaker by more than 30% and resulting in improved team cohesion and communication.", 
+                        "Monitored and resolved various operator issues and system malfunctions, reducing machine downtime and increasing productivity through quick response times and troubleshooting endeavors."]}
+                    />
+                <PositionContainer 
+                    positionTitle="Crew Leader"
+                    startDate="Aug 2024"
+                    endDate="May 2025"
+                    companyName="Johnny's Markets"
+                    location="Angola, IN"
+                    bullets={["Interviewed and trained five employees while collaborating with a team of 14, increasing morale and reducing work related stress on team members by more than 25% by improving workload distribution.",
+                        "Managed routine store operations in place of management, allowing for an increase in productivity and a 15% reduction in labor costs due to reduced managerial work hours.", 
+                        "Collaborated with team members to perform quarterly deep cleaning runs of the worksite, resulting in multiple 98%+ scores on white-glove inspections."]}
+                    />
+                <PositionContainer 
+                    positionTitle="CIT Student"
+                    startDate="Aug 2022"
+                    endDate="May 2024"
+                    companyName="Jackson Area Career Center"
+                    location="Jackson, MI"
+                    bullets={["Crafted unique and entertaining projects in a small team of peers, using Python to structure game logic, graphics, and sound to create fully functional games including a 2D-platformer, snake game, and more.",
+                        "Architected personal web projects using HTML, CSS, and JavaScript to create interactive and visually appealing sites, including a timer webapp, treasure hunt game, madlibs generator, and more.", 
+                        "Developed a singleplayer, third-person game using Unreal Engine 4/5, implementing complex enemy AI, player movement and ability systems, and static level design to create a fun and satisfying horde-style melee fighting gameplay experience."]}
+                    last={true}
+                    />
             </div>
+            </div>
+            <span className="hero-spacer"></span>
         </BaseSection>
     )
 }
