fed7297212ae931b626a00041facc1f0920ee05a
JDubbs89 <152918687+JDubbs89@users.noreply.github.com>
2025-09-29 12:33:13 -0400

    Updated project dynamic route styles, content container px to reflect good responsive design

--- app/(site)/projects/[projectId]/page.tsx
+++ app/(site)/projects/[projectId]/page.tsx
@@ -12,14 +12,13 @@ export default async function ProjectDetails({
 }) {
   const project = get_project((await params).projectId.replaceAll("%20", " "));
   return (
-    <div className="mt-10 flex flex-col gap-5">
-      <div className="flex justify-between mx-auto gap-[30%] w-[70%]">
-        <BackButton className="ghost-button mx-auto w-fit text-[2vh]"/>
-        {project.interactUrl != "" ? <Link href={project.interactUrl} className="ghost-button mx-auto w-fit text-[2vh]">{project.interactText}</Link> : null}
+    <div className="project-section">
+      <div className="project-buttons-container">
+        <BackButton className={`ghost-button project-back-button`}/>
+        {project.interactUrl != "" ? <Link href={project.interactUrl} className="ghost-button project-view-button">{project.interactText}</Link> : null}
       </div>
-      <Image src={project.imageUrl} alt={`${project.id} Image`} height={900} width={1600} className="mx-auto h-fit w-[50vw] rounded-2xl"/>
+      <Image src={project.imageUrl} alt={`${project.id} Image`} height={900} width={1600} className="project-image"/>
       <SummarySection className='timeline-container content-wrapper-px w-full mb-10' title={project.id}>
-        
         <div className="summary-section-para my-4">
           {project.desc}
         </div>
